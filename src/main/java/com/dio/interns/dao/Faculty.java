358bf30aebbab227760b5101483aba1ea5fccafa
kislo_metal <kislo.metal@gmail.com>
2013-01-03 16:11:19 +0200

    adding excludes

---
+++ src/main/java/com/dio/interns/dao/Faculty.java
@@ -0,0 +1,36 @@
+package com.dio.interns.dao;
+
+import java.io.Serializable;
+
+public class Faculty implements Serializable {
+    private static final long serialVersionUID = -4034184535404994684L;
+
+    private Long id;
+    private String name;
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
