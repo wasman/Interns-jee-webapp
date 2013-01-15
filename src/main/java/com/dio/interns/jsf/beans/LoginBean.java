de03a2f80741ce1560e26503ccea933dc1ba68b9
kislo_metal <kislo.metal@gmail.com>
2013-01-15 17:40:02 +0200

    adding spring security, fixing navigation

--- src/main/java/com/dio/interns/jsf/beans/LoginBean.java
+++ src/main/java/com/dio/interns/jsf/beans/LoginBean.java
@@ -1,40 +1,104 @@
 package com.dio.interns.jsf.beans;
 
-import java.io.Serializable;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.WebAttributes;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.faces.event.PhaseEvent;
+import javax.faces.event.PhaseId;
+import javax.faces.event.PhaseListener;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+import java.io.Serializable;
 
-@ManagedBean(name="loginBean")
+@ManagedBean(name = "loginBean")
 @SessionScoped
-public class LoginBean implements Serializable {
-	private static final long serialVersionUID = 7024559864889886875L;
+public class LoginBean implements Serializable, PhaseListener {
+    private static final long serialVersionUID = 7024559864889886875L;
 
-	private String user;
-	private String login;
-	private String password;
-	
-	public String doLogin() {
-		user = login;
-		return "success";
-	}
-	
-	public String getUser() {
-		return user;
-	}
-	public void setUser(String user) {
-		this.user = user;
-	}
-	public String getLogin() {
-		return login;
-	}
-	public void setLogin(String login) {
-		this.login = login;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    private String user;
+    private String login;
+    private String password;
+
+    public String doLogin() throws ServletException, IOException {
+        ExternalContext context = FacesContext.getCurrentInstance().getExternalContext();
+
+        RequestDispatcher dispatcher = ((ServletRequest) context.getRequest())
+                .getRequestDispatcher("/j_spring_security_check");
+
+        dispatcher.forward((ServletRequest) context.getRequest(),
+                (ServletResponse) context.getResponse());
+
+        FacesContext.getCurrentInstance().responseComplete();
+
+        this.user = context.getRemoteUser();
+        return user != null ? "j_spring_security_check_ok" : "j_spring_security_check_fail";
+    }
+
+    public String doLogout() {
+        SecurityContextHolder.clearContext();
+        return "j_spring_security_logout";
+    }
+
+
+    @Override
+    public void afterPhase(PhaseEvent phaseEvent) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void beforePhase(PhaseEvent event) {
+        Exception e = (Exception) FacesContext.getCurrentInstance().
+                getExternalContext().getSessionMap().get(WebAttributes.AUTHENTICATION_EXCEPTION);
+
+        if (e instanceof BadCredentialsException) {
+//            logger.debug("Found exception in session map: "+e);
+            FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put(
+                    WebAttributes.AUTHENTICATION_EXCEPTION, null);
+            FacesContext.getCurrentInstance().addMessage(null,
+                    new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                            "Username or password not valid.", "Username or password not valid"));
+        }
+    }
+
+    public PhaseId getPhaseId() {
+        return PhaseId.RENDER_RESPONSE;
+    }
+
+    public String doLogin2() {
+        user = login;
+        return "success";
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
